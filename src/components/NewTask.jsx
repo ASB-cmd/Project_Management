4902af0d34e77b1670975a93d7e839d5319f4820
Akshay Bhope <akshay.bhope@sjsu.edu>
2024-05-15 22:22:04 -0700

    initial commit

---
+++ src/components/NewTask.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export default function NewTask({ onAdd }) {
+  const [enteredTask, setEnteredTask] = useState("");
+
+  function handleChange(event) {
+    setEnteredTask(event.target.value);
+  }
+
+  function handleClick() {
+    if (enteredTask.trim() === "") {
+      return;
+    }
+    onAdd(enteredTask);
+    setEnteredTask("");
+  }
+
+  return (
+    <div className="flex items-center gap-4">
+      <input
+        type="text"
+        className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+        onChange={handleChange}
+        value={enteredTask}
+      />
+      <button
+        className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-300 hover:bg-stone-600 hover:text-stone-100"
+        onClick={handleClick}
+      >
+        Add Task
+      </button>
+    </div>
+  );
+}
